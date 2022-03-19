68e2f630fe954cf714ace2275de9c0f2755cf3f4
kiketion <kiketionje@gmail.com>
2022-03-19 15:04:05 +0300

    todos on console

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,21 @@ import TodoForm from './TodoForm';
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
   return (
     <div>
       <h1>whats the plan for today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
